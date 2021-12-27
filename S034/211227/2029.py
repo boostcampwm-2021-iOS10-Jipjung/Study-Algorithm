60464fb3832e796d94a22a68e364585324e61a3d
starpro123 <starpro123@naver.com>
2021-12-27 12:21:24 +0900

    2029 - discussion 카피

--- S034/211227/2029.py
+++ S034/211227/2029.py
@@ -1,30 +1,9 @@
+from collections import Counter
+
 class Solution:
     def stoneGameIX(self, stones: List[int]) -> bool:
-        remains = [[] for i in range(3)]
-        for stone in stones:
-            if stone % 3 == 0:
-                remains[0].append(stone)
-            elif stone % 3 == 1:
-                remains[1].append(stone)
-            else:
-                remains[2].append(stone)
-        turn = False
-        left_count = len(stones)
-        total_remain = 0
-        while True:
-            for i in range(3):
-                if i + total_remain == 3:
-                    continue
-                if len(remains[i]) > 0:
-                    remains[i].pop()
-                    total_remain = (total_remain + i) % 3
-                    left_count -= 1
-                    if left_count == 0:
-                        return False
-                    break
-            else:
-                break
-            turn = not turn
-        print(remains)
-        return turn
+        remains = collections.Counter(a % 3 for a in stones)
+        if min(remains[1], remains[2]) == 0:
+            return max(remains[1], remains[2]) > 2 and remains[0] % 2 > 0
+        return abs(remains[1] - remains[2]) > 2 or remains[0] % 2 == 0
             
