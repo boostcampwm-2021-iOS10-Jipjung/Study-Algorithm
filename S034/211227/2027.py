6f28f9e8633d72f252a4aef917dc54fb539982a1
starpro123 <starpro123@naver.com>
2021-12-27 11:18:33 +0900

    2027 - 요구사항 파악 중간중간에 끊겨도됨

--- S034/211227/2027.py
+++ S034/211227/2027.py
@@ -2,15 +2,14 @@ class Solution:
     def minimumMoves(self, s: str) -> int:
         o_count = 0
         move = 0
-        before_index = -1
+        count = 0
         for i in range(len(s)):
-            if s[i] == "O":
-                o_count += 1
-            if i - before_index == 3:
-                if o_count < 3:
-                    move += 1
-                before_index = i
-                o_count = 0
-            if i == len(s) - 1 and i - before_index > o_count:
+            if count == 0 and s[i] == "O":
+                continue
+            count += 1
+            if count == 3:
                 move += 1
+                count = 0
+        if count > 0 and move > 0:
+            move += 1
         return move
