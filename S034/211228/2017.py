3ad445938568f0bcfef77238c0c7617dde145abd
starpro123 <starpro123@naver.com>
2021-12-28 11:36:27 +0900

    2017 - 2path로 계산만 하면되는 문제, 위 아래 prefix를 해둔다.

--- S034/211228/2017.py
+++ S034/211228/2017.py
@@ -1,32 +1,10 @@
-from collections import deque
-
 class Solution:
-    DIRECTION = ((1, 0), (0, 1))
     def gridGame(self, grid: List[List[int]]) -> int:
-        b_scores = []
-        next_grid = [grid[0][:], [0] * len(grid[0])]
-        next_grid[0][0] = 0
-        self.find_max(next_grid)
-        
-        b_scores.append(self.find_max(next_grid))
+        bottom_route = 0
+        top_route = sum(grid[0]) - grid[0][0]
+        answer = top_route
         for j in range(1, len(grid[0])):
-            next_grid[0][j] = 0
-            next_grid[1][j - 1] = grid[1][j - 1]
-            b_scores.append(self.find_max(next_grid))
-        # print(b_scores)    
-        return min(b_scores)
-    
-    def find_max(self, grid):
-        q = deque()
-        q.append((0, 0, 0))
-        answer = []
-        while q:
-            i, j, v = q.popleft()
-            if i == len(grid) - 1 and j == len(grid[0]) - 1:
-                 answer.append(v)
-            for di, dj in self.DIRECTION:
-                 ni, nj = i + di, j + dj
-                 if 0 > ni or ni >= len(grid) or 0 > nj or nj >= len(grid[0]):
-                    continue
-                 q.append((ni, nj, grid[ni][nj] + v))
-        return max(answer)
+            top_route -= grid[0][j]
+            bottom_route += grid[1][j - 1]
+            answer = min(answer, max(top_route, bottom_route))   
+        return answer
