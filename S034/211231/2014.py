bfe77b055a7160e21e9eee307790bab458f335cf
starpro123 <starpro123@naver.com>
2022-01-02 18:53:13 +0900

    일단 시간초과는 하지만 로우하게 코드를 짜봄

---
+++ S034/211231/2014.py
@@ -0,0 +1,30 @@
+from collections import Counter, deque
+
+class Solution:
+    def longestSubsequenceRepeatedK(self, s: str, k: int) -> str:
+        cands = list("".join(char * (count // k) for char, count in Counter(s).items()))
+        cands.sort()
+        
+        def is_repeated(seq):
+            count = 0
+            i = 0
+            n = len(seq)
+            for char in s:
+                if seq[i] == char:
+                    i += 1
+                if i == n:
+                    count += 1
+                    i = 0
+                if count == k:
+                    return True
+        
+        q = deque([""])
+        answer = ""
+        while q:
+            sub_seq = q.popleft()
+            for cand in cands:
+                new_sub_seq = sub_seq + cand
+                if is_repeated(new_sub_seq):
+                    q.append(new_sub_seq)
+                    answer = new_sub_seq
+        return answer
